7073d49d3277a0a36b932d99dbfcc3ec3785586e
jaykay233 <zhiyuanx@usc.edu>
2021-04-05 11:48:14 +0800

    add

---
+++ AuthTokenizer/src/CredentialStorage.java
@@ -0,0 +1,5 @@
+public class CredentialStorage {
+    public static String getPasswordByAppId(String appId){
+
+    }
+}
---
+++ AuthTokenizer/src/apiRequest.java
@@ -0,0 +1,31 @@
+public class apiRequest {
+    private String baseUrl;
+    private String token;
+    private String appId;
+    private long timestamp;
+
+    public apiRequest(String baseUrl, String token, String appId, long timestamp){
+
+    }
+
+    public static apiRequest createFromFullUrl(String url){
+
+    }
+
+    public String getBaseUrl(){
+
+    }
+
+    public String getToken(){
+
+    }
+
+    public String getAppId(){
+
+    }
+
+    public long getTimestamp(){
+
+    }
+
+}
